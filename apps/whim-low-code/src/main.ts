41916e118799d6bae85032a7f965c2d76c36100c
hsbtr <hsbtr@qq.com>
2025-04-12 19:08:25 +0800

    feat: 全局环境配置；项目文档

--- apps/whim-low-code/src/main.ts
+++ apps/whim-low-code/src/main.ts
@@ -6,14 +6,25 @@ import {
   AllExceptionsFilter,
   HttpExceptionFilter,
 } from '@comm/comm';
+import { generateDocument } from './doc';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   app.enableVersioning({
     type: VersioningType.URI,
   });
-  app.useGlobalInterceptors(new TransformInterceptor());
-  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  generateDocument(app);
+
+  // 添加热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   await app.listen(3000);
 }
